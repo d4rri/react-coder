707d2e95b4d6f9a6a7b1b80ea7c9b0b82ddb8c26
Agustin Darricau <agus_treslomas137@hotmail.com>
2022-07-16 14:56:05 -0300

    react-coder

--- js/carrito.js
+++ js/carrito.js
@@ -5,13 +5,44 @@ let btnAgregar4 = document.querySelector("#carrito4")
 let btnAgregar5 = document.querySelector("#carrito5")
 let btnAgregar6 = document.querySelector("#carrito6")
 
-
+const recuperarJSON = (URL) => {   
+    let filas = "";
+    let prod="";
+    let total = "";
+    let preciof = "";
+    let texto =`<p class="btn form3 text-light" type="button">Precio final de mi compra:`
+    debugger
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+            contenidoJSON = data
+            contenidoJSON.forEach(prod => {; 
+                total=new Producto(prod.producto,prod.precio,prod.iva);
+                filas += `<ul class="list-group mb-3">
+                        <li class="list-group-item d-flex justify-content-between lh-sm">
+                        <div>
+                            <h6 class="my-0">${prod.producto || "el producto no existe"}</h6>
+                            <small class="">${prod.iva || "el iva no existe"}</small>
+                        </div>
+                            <span class="">${prod.precio || "el precio no existe"}</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between">
+                        <p>Total del producto (Pesos Argentinos):</p>
+                    <strong>${total.precioFinal() || "el precio final no existe"}</strong>
+                    </li>
+                    </ul>`
+                    preciof = (parseInt(preciof + total.precioFinal()));
+                    document.getElementById('verCompra').innerHTML = filas;
+                    document.getElementById('verPrecioFinal').innerHTML = texto + preciof;                                                       
+            })
+        })
+    }
     
 btnAgregar1.addEventListener("click", () => {
-    llenarCarrito.push(new Producto("Curso de auto-maquillaje", 4000, iva,))
-    localStorage.setItem("carrito", JSON.stringify(llenarCarrito))
+    recuperarJSON(URL)
     alertar("Se agrego el producto al carrito.")
     })
+
 
 
 btnAgregar2.addEventListener("click", () => {
--- js/vercompra.js
+++ js/vercompra.js
@@ -16,37 +16,35 @@ function alertar(mensaje) {
 }
 
 
-const recuperarJSON = (URL) => {   
-    let filas = "";
-    let prod="";
-    let total = "";
-    let preciof = "";
-    let texto =`<p class="btn form3 text-light" type="button">Precio final de mi compra:`
-    debugger
-    fetch(URL)
-        .then(response => response.json())
-        .then(data => {
-            contenidoJSON = data
-            contenidoJSON.forEach(prod => {; 
-                total=new Producto(prod.producto,prod.precio,prod.iva);
-                filas += `<ul class="list-group mb-3">
-                        <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <div>
-                            <h6 class="my-0">${prod.producto || "el producto no existe"}</h6>
-                            <small class="">${prod.iva || "el iva no existe"}</small>
-                        </div>
-                            <span class="">${prod.precio || "el precio no existe"}</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between">
-                        <p>Total del producto (Pesos Argentinos):</p>
-                    <strong>${total.precioFinal() || "el precio final no existe"}</strong>
-                    </li>
-                    </ul>`
-                    preciof = (parseInt(preciof + total.precioFinal()));
-                    document.getElementById('verCompra').innerHTML = filas;
-                    document.getElementById('verPrecioFinal').innerHTML = texto + preciof;                                                       
-            })
-        })
-    }
-
-recuperarJSON(URL)
+// const recuperarJSON = (URL) => {   
+//     let filas = "";
+//     let prod="";
+//     let total = "";
+//     let preciof = "";
+//     let texto =`<p class="btn form3 text-light" type="button">Precio final de mi compra:`
+//     debugger
+//     fetch(URL)
+//         .then(response => response.json())
+//         .then(data => {
+//             contenidoJSON = data
+//             contenidoJSON.forEach(prod => {; 
+//                 total=new Producto(prod.producto,prod.precio,prod.iva);
+//                 filas += `<ul class="list-group mb-3">
+//                         <li class="list-group-item d-flex justify-content-between lh-sm">
+//                         <div>
+//                             <h6 class="my-0">${prod.producto || "el producto no existe"}</h6>
+//                             <small class="">${prod.iva || "el iva no existe"}</small>
+//                         </div>
+//                             <span class="">${prod.precio || "el precio no existe"}</span>
+//                         </li>
+//                         <li class="list-group-item d-flex justify-content-between">
+//                         <p>Total del producto (Pesos Argentinos):</p>
+//                     <strong>${total.precioFinal() || "el precio final no existe"}</strong>
+//                     </li>
+//                     </ul>`
+//                     preciof = (parseInt(preciof + total.precioFinal()));
+//                     document.getElementById('verCompra').innerHTML = filas;
+//                     document.getElementById('verPrecioFinal').innerHTML = texto + preciof;                                                       
+//             })
+//         })
+//     }
